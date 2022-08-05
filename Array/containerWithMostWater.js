2d0b34fd2f99d4713e41407b79510366a810b9f3
Omkar <omkarbaacha@foreignadmits.com>
2022-08-05 23:41:34 +0530

    Container with most water

--- Array/containerWithMostWater.js
+++ Array/containerWithMostWater.js
@@ -20,3 +20,23 @@ function maxArea(height) {
 
 console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
 console.log(Math.min(1,7))
+
+const maxArea1 = (height) => {
+  let maxArea = 0;
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right) {
+    let currentArea = Math.min(height[left], height[right]) * (right - left);
+
+    maxArea = Math.max(currentArea, maxArea);
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return maxArea;
+};
+
+maxArea1([1,8,6,2,5,4,8,3,7]);
+console.log(maxArea1([1,8,6,2,5,4,8,3,7]))
